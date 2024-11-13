d5a2833f0a06c21ea4c63508cc735dd1b3cd045d
n3land <40582510+n3land@users.noreply.github.com>
2024-11-13 10:50:40 -0600

    Revert "Technest 56/listado prendas"

--- src/main/java/com/example/vcloset/logic/entity/clothing/ClothingRepository.java
+++ src/main/java/com/example/vcloset/logic/entity/clothing/ClothingRepository.java
@@ -3,19 +3,8 @@ package com.example.vcloset.logic.entity.clothing;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.query.Procedure;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface ClothingRepository extends JpaRepository<Clothing, Long> {
 
-    Page<Clothing> findByUserId(Long userId, Pageable pageable);
-
-    Page<Clothing> findByUserIdAndIsFavoriteTrue(Long userId, Pageable pageable);
-
-    @Procedure(name = "GetClothingByUserAndType")
-    List<Clothing> getClothingByUserAndType(
-            @Param("userIdParam") Long userId,
-            @Param("typeParam") String type);
+    Page<Clothing> getOrderByUserId(Long id, Pageable pageable);
 }
